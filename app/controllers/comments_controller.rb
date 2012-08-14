54ca11069ba2013dce99930b3dd167b6e3e27276
Rafael Garbin <rgarbin@gmail.com>
2012-08-13 21:10:23 -0300

    Otimizando a model para deixar mais clean code.
    Signed-off-by: Rafael Garbin <rgarbin@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,85 +3,41 @@ class CommentsController < ApplicationController
   before_filter :authenticate
   respond_to :html, :json, :js
   
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @comments }
-    end
+    respond_with @comments
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @comment }
-    end
+    respond_with @comment
   end
 
-  # GET /comments/new
-  # GET /comments/new.json
   def new
     @comment = Comment.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @comment }
-    end
+    respond_with @comment
   end
 
-  # GET /comments/1/edit
   def edit
     @comment = Comment.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
     @comment.user = current_user
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.js
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Comment was successfully created.' if @comment.save
+    respond_with @comment
   end
 
-  # PUT /comments/1
-  # PUT /comments/1.json
   def update
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Comment was successfully updated.' if @comment.update_attributes(params[:comment])                                                   
+    respond_with @comment
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.js
-    end
+    respond_with @comment
   end
 end
