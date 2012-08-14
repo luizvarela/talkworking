b53447c9f376f8043c1be97a8631d124b7ae89e2
Leonardo Goularte Siqueira <leonardo.prg@gmail.com>
2012-08-13 21:56:19 -0300

    fix bug when user login is not ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     	session[:user_id] = user.id
 			redirect_to root_path, :notice =>  I18n.t('sessions.messages.login_successfully')
 		else
+		  @user = User.new
 			flash.now[:alert] = I18n.t('sessions.messages.login_error')
 			render :action => :new
 		end
